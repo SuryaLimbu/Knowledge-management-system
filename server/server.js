60fbfc0363f3b1dbd0fcba53aa72fafc85a8cd26
Surya Man Kedem <46651550+SuryaLimbu@users.noreply.github.com>
2024-01-23 11:32:41 +0000

    update exiting

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const connectDB = require("./db"); // Change the path as needed
 const cors = require("cors");
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const { authenticateToken } = require('./middlewares/authenticateToken');
@@ -15,22 +16,35 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 
-
+// const DATABASE_URL = process.env.DATABASE_URL;
 
 
 // Your routes and other middleware go here...
 app.get('/', function (req, res) {
   res.type('text/plain');
   res.status(200);
+
   // res.send('Welcome to MongoDB API');
-  if(connectDB()){
+  if (connectDB()) {
     res.send('DB connected.')
   }
-  else{
+  else {
     res.send('DB connection fail!')
   }
 
 })
+///
+
+// LOGIN
+const loginRouter = require('./routers/loginRoutes');
+
+app.use('/api',loginRouter);
+
+const tokenRouter = require('./routers/tokenRoutes');
+app.use('/api', tokenRouter);
+
+const logoutRouter = require('./routers/logoutRoutes');
+app.use('/api', logoutRouter);
 
 
 // applying middleware to all routes
